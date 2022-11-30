c45f2ab5ca2a37d1057529c61be0b61eb36d05b6
Ahmed Zaki <ahmad98zaki@gmail.com>
2022-11-30 20:47:26 +0200

    Some files

---
+++ lib/screens/hadeeth/ahadeth_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/screens/hadeeth/hadeeth_details_screen.dart';
+import 'package:islami_app/themes/my_theme.dart';
+
+class AhadethScreen extends StatefulWidget {
+  @override
+  State<AhadethScreen> createState() => _AhadethScreenState();
+}
+
+class _AhadethScreenState extends State<AhadethScreen> {
+  List<HadeethData> ahadeth = [];
+
+  @override
+  Widget build(BuildContext context) {
+    if (ahadeth.isEmpty) {
+      loadAhadethFile();
+    }
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Image.asset('assets/images/ahadeth_image.png'),
+          Divider(color: MyThemeData.colorGold, thickness: 1.0),
+          Text(
+            'الأحاديث',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Divider(color: MyThemeData.colorGold, thickness: 1.0),
+          ahadeth.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : Expanded(
+                  child: ListView.separated(
+                    separatorBuilder: (context, index) => Divider(
+                      color: MyThemeData.colorGold,
+                      thickness: 0.5,
+                      indent: 20.0,
+                      endIndent: 20.0,
+                    ),
+                    itemCount: ahadeth.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, HadeethDetailsScreen.routeName, arguments: ahadeth[index]);
+                        },
+                        child: Text(
+                          ahadeth[index].title,
+                          style: Theme.of(context).textTheme.subtitle1,
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    },
+                  ),
+                )
+        ],
+      ),
+    );
+  }
+
+  void loadAhadethFile() async {
+    String ahadethContent =
+        await rootBundle.loadString('assets/files/ahadeth.txt');
+    List<String> allAhadeth = ahadethContent.trim().split('#\r\n');
+    for (int i = 0; i < allAhadeth.length; i++) {
+      String hadeeth = allAhadeth[i];
+      List<String> hadeethLines = hadeeth.split('\n');
+      String title = hadeethLines[0];
+      hadeethLines.removeAt(0);
+      HadeethData hadeethData = HadeethData(title, hadeethLines);
+      ahadeth.add(hadeethData);
+      setState(() {});
+    }
+  }
+}
+
+class HadeethData {
+  String title;
+  List<String> content;
+
+  HadeethData(this.title, this.content);
+}
